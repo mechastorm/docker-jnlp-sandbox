ae960e54f22a551475a23511bece4544696d1902
Shih Oon Liong <shih.liong@samsung.com>
2018-08-23 12:23:22 -0700

    Push update

--- push.sh
+++ push.sh
@@ -1,33 +1,35 @@
 #!/bin/bash
 
 # Verify labels - docker image inspect jenkins-master-base | jq -r '.[0].Config.Labels'
-
+SERVICE_NAME=jnlp-slave-sandbox
 DO_DOCKER_LOGIN="1"
 VERBOSE="0"
 DRY_RUN="0"
-TAG=""
+DO_GIT_TAG="0"
+BASE_TAG=""
 NAMESPACE=""
 
 while true; do
   case "$1" in
     -v | --verbose ) VERBOSE="1"; shift ;;
     -n | --dry-run ) DRY_RUN="1"; shift ;;
+    -g | --git-tag ) DO_GIT_TAG="1"; shift ;;
     --skip-login ) DO_DOCKER_LOGIN="0"; shift ;;
     -r | --registry ) REGISTRY="$2"; shift; shift ;;
     -n | --namespace ) NAMESPACE="$2"; shift; shift ;;
-    -t | --tag ) TAG="$2"; shift; shift ;;
+    -t | --tag ) BASE_TAG="$2"; shift; shift ;;
     -- ) shift; break ;;
     * ) break ;;
   esac
 done
 
 # If tag is empty, set it to latest
-if [ -z "$TAG" ];
+if [ -z "$BASE_TAG" ];
 then
-    TAG=":latest"
-else
-    TAG=":"$TAG
+    BASE_TAG="latest"
 fi
+TAG=":"$BASE_TAG
+
 
 # If namespace is not empty, set it to latest
 if [ -z "$NAMESPACE" ];
@@ -40,7 +42,8 @@ fi
 DATE_TIME=`date '+%Y%m%d%H%M%S'`
 COMMIT_ID=$(git rev-parse HEAD)
 
-if [ "$VERBOSE" -eq "1" ]; then
+if [ "$VERBOSE" -eq "1" ];
+then
     echo VERBOSE=$VERBOSE
     echo DO_DOCKER_LOGIN=$DO_DOCKER_LOGIN
     echo REGISTRY=$REGISTRY
@@ -50,12 +53,21 @@ if [ "$VERBOSE" -eq "1" ]; then
     echo COMMIT_ID=$COMMIT_ID
 fi
 
-# docker registry login
-if [ "$DO_DOCKER_LOGIN" -eq "1" ];
+if [ "$DRY_RUN" -eq "0" ];
 then
-   docker login $REGISTRY
+    # docker registry login
+    if [ "$DO_DOCKER_LOGIN" -eq "1" ];
+    then
+       docker login $REGISTRY
+    fi
+
+    DOCKER_REGISTRY_URL=$REGISTRY DOCKER_REGISTRY_NAMESPACE=$NAMESPACE IMAGE_TAG=$TAG TIMESTAMP=$DATE_TIME COMMIT_ID=$COMMIT_ID docker-compose build
+
+    DOCKER_REGISTRY_URL=$REGISTRY DOCKER_REGISTRY_NAMESPACE=$NAMESPACE IMAGE_TAG=$TAG TIMESTAMP=$DATE_TIME COMMIT_ID=$COMMIT_ID docker-compose push $SERVICE_NAME
+
+    if [ "$DO_GIT_TAG" -eq "1" ];
+    then
+        git tag $TAG
+        git push push origin $TAG
+    fi
 fi
-
-DOCKER_REGISTRY_URL=$REGISTRY DOCKER_REGISTRY_NAMESPACE=$NAMESPACE IMAGE_TAG=:latest TIMESTAMP=$DATE_TIME COMMIT_ID=$COMMIT_ID docker-compose build
-
-DOCKER_REGISTRY_URL=$REGISTRY DOCKER_REGISTRY_NAMESPACE=$NAMESPACE IMAGE_TAG=:latest TIMESTAMP=$DATE_TIME COMMIT_ID=$COMMIT_ID docker-compose push jnlp-slave-sandbox
